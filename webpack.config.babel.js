f49de27497e8c8b8ada007dbbdfcca6df5c7758f
Amin NAIRI <nairi.amin@gmail.com>
2018-06-27 21:54:37 +0200

    :sparkles: added initial entry configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,1 +1,12 @@
-export default {}
+import path from 'path'
+
+export default {
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js')
+  },
+
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
