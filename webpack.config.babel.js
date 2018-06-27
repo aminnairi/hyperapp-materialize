dde2d4a0c5ea7281a3b1a190e6a0b293fab37e38
Amin NAIRI <nairi.amin@gmail.com>
2018-06-27 22:17:24 +0200

    :sparkles: added what's necessary to build jsx correctly

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,13 +2,27 @@ import path from 'path'
 
 export default {
   mode: 'development',
-  
+
   entry: {
-    index: path.resolve(__dirname, 'src', 'index.js')
+    index: path.resolve(__dirname, 'src', 'index.jsx')
   },
 
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
+  },
+
+  externals: {
+    hyperapp: 'hyperapp'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx/,
+        include: /src/,
+        loader: 'babel-loader'
+      }
+    ]
   }
 }
