79f48b419cfaa7d37ef64aa65f91f79bdf6bcd30
Amin NAIRI <nairi.amin@gmail.com>
2018-06-28 21:26:34 +0200

    :sparkles: added column computation

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,14 +1,30 @@
 import { h } from 'hyperapp'
 
-const Column = () => {
+const col = (prefix, value) => {
+
+  if (value && 1 <= value && value <= 12) {
+
+    return `${prefix}${value}`
+
+  }
+
+  return ''
+
+}
+
+export const Column = ({ small, medium, large, extraLarge, className, ...attributes }, children) => {
 
   return () => {
 
-    const classes = {}
+    const classes = `col ${col('s', small)} ${col('m', medium)} ${col('l', large)} ${col('xl', extraLarge)}`.trim()
 
     return (
 
-      <div className={ classes }></div>
+      <div className={ classes } { ...attributes }>
+
+        { children }
+
+      </div>
 
     )
 
