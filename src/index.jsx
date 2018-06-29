5abfcad299fb183b7113d4a45310d35942798af9
Amin NAIRI <nairi.amin@gmail.com>
2018-06-29 21:30:20 +0200

    :bug: fixed relative path to video component

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { Column }         from './components/Column.jsx'
 import { Container }      from './components/Container.jsx'
 import { Image }          from './components/Image.jsx'
 import { Row }            from './components/Row.jsx'
-import { Video }          from './components/VideoMedia.jsx'
+import { Video }          from './components/Video.jsx'
 import { VideoContainer } from './components/VideoContainer.jsx'
 
 export {
