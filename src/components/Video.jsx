5e6c1dba6f1e687c6b8fafaea5108094ffdd3c93
Amin NAIRI <nairi.amin@gmail.com>
2018-06-29 21:37:38 +0200

    :sparkles: added better classes resolution

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,9 +4,19 @@ export const Video = ({ responsive, className, ...props }, children) => {
 
   return () => {
 
+    const classes = []
+
+    classes.push(className)
+
+    if (responsive) {
+
+      classes.push('responsive-video')
+
+    }
+
     return (
 
-      <video className={ responsive ? 'responsive-video' : '' + className } { ...props }>
+      <video className={ classes.join(' ') } { ...props }>
 
         { children }
 
