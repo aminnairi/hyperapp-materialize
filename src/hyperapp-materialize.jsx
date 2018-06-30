6ac3a87ffa1ca1664f68e00c232226aca7d4f034
Amin NAIRI <nairi.amin@gmail.com>
2018-06-30 11:01:24 +0200

    :sparkles: renamed

--- src/hyperapp-materialize.jsx
+++ src/hyperapp-materialize.jsx
@@ -1,3 +1,4 @@
+import { Badge }          from './components/Badge.jsx'
 import { Column }         from './components/Column.jsx'
 import { Container }      from './components/Container.jsx'
 import { Image }          from './components/Image.jsx'
@@ -7,6 +8,7 @@ import { Video }          from './components/Video.jsx'
 import { VideoContainer } from './components/VideoContainer.jsx'
 
 export {
+  Badge,
   Column,
   Container,
   Image,
