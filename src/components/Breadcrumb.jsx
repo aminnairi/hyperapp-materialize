2323b25b7cd4381e7e62cabaee927be50fa459c4
Amin NAIRI <nairi.amin@gmail.com>
2018-06-30 22:34:10 +0200

    :sparkles: renamed to Breadcrumb

---
+++ src/components/Breadcrumb.jsx
@@ -0,0 +1,53 @@
+import { h } from 'hyperapp'
+
+export const Breadcrumbs = (props, children) => {
+
+  return () => {
+
+    return (
+
+      <nav { ...props }>
+
+        <div className='nav-wrapper'>
+
+          <div className='col s12'>
+
+            { children }
+
+          </div>
+
+        </div>
+
+      </nav>
+
+    )
+
+  }
+
+}
+
+export const Breadcrumb = ({ href, className, ...props }, children) => {
+
+  return () => {
+
+    const classes = []
+
+    if (className) {
+
+      classes.push(className)
+
+    }
+
+    return (
+
+      <a href={ href } className={ classes.join(' ') } { ...props }>
+
+        { children }
+
+      </a>
+
+    )
+
+  }
+
+}
