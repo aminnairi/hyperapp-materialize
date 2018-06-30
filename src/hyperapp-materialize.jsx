a24255c9bae6d3ac5b98ec645167ee1cee092fbf
Amin NAIRI <nairi.amin@gmail.com>
2018-06-30 22:34:25 +0200

    :sparkles: added breadcrumb component to export

--- src/hyperapp-materialize.jsx
+++ src/hyperapp-materialize.jsx
@@ -1,15 +1,18 @@
-import { Badge }          from './components/Badge.jsx'
-import { Button }         from './components/Button.jsx'
-import { Column }         from './components/Column.jsx'
-import { Container }      from './components/Container.jsx'
-import { Image }          from './components/Image.jsx'
-import { Row }            from './components/Row.jsx'
-import { Table }          from './components/Table.jsx'
-import { Video }          from './components/Video.jsx'
-import { VideoContainer } from './components/VideoContainer.jsx'
+import { Badge }                    from './components/Badge.jsx'
+import { Breadcrumbs, Breadcrumb }  from './components/Breadcrumb.jsx'
+import { Button }                   from './components/Button.jsx'
+import { Column }                   from './components/Column.jsx'
+import { Container }                from './components/Container.jsx'
+import { Image }                    from './components/Image.jsx'
+import { Row }                      from './components/Row.jsx'
+import { Table }                    from './components/Table.jsx'
+import { Video }                    from './components/Video.jsx'
+import { VideoContainer }           from './components/VideoContainer.jsx'
 
 export {
   Badge,
+  Breadcrumb,
+  Breadcrumbs,
   Button,
   Column,
   Container,
