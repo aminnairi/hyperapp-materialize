8b562630ea38690c8ea30f0fe08bdc6d11b99c4a
Amin NAIRI <nairi.amin@gmail.com>
2018-07-01 18:12:29 +0200

    :sparkles: added card content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,10 +9,38 @@ export const Card = ({ className, ...props }, children) => {
     if (className) {
 
       classes.push(className)
-      
+
     }
 
     classes.push('card')
+
+    return (
+
+      <div className={ classes.join(' ') } { ...props }>
+
+        { children }
+
+      </div>
+
+    )
+
+  }
+
+}
+
+export const CardContent = ({ className, ...props }, children) => {
+
+  return () => {
+
+    const classes = []
+
+    if (className) {
+
+      classes.push(className)
+
+    }
+
+    classes.push('card-content')
 
     return (
 
