6b497587e17aa39ae7376fec9fd85989ba684014
Amin NAIRI <nairi.amin@gmail.com>
2018-07-01 18:45:39 +0200

    :sparkles: added export for card reveal

--- src/hyperapp-materialize.jsx
+++ src/hyperapp-materialize.jsx
@@ -1,14 +1,14 @@
-import { Badge }                                                from './components/Badge.jsx'
-import { Breadcrumbs, Breadcrumb }                              from './components/Breadcrumb.jsx'
-import { Button }                                               from './components/Button.jsx'
-import { Card, CardAction, CardContent, CardImage, CardTitle }  from './components/Card.jsx'
-import { Column }                                               from './components/Column.jsx'
-import { Container }                                            from './components/Container.jsx'
-import { Image }                                                from './components/Image.jsx'
-import { Row }                                                  from './components/Row.jsx'
-import { Table }                                                from './components/Table.jsx'
-import { Video }                                                from './components/Video.jsx'
-import { VideoContainer }                                       from './components/VideoContainer.jsx'
+import { Badge }                                                            from './components/Badge.jsx'
+import { Breadcrumbs, Breadcrumb }                                          from './components/Breadcrumb.jsx'
+import { Button }                                                           from './components/Button.jsx'
+import { Card, CardAction, CardContent, CardImage, CardReveal, CardTitle }  from './components/Card.jsx'
+import { Column }                                                           from './components/Column.jsx'
+import { Container }                                                        from './components/Container.jsx'
+import { Image }                                                            from './components/Image.jsx'
+import { Row }                                                              from './components/Row.jsx'
+import { Table }                                                            from './components/Table.jsx'
+import { Video }                                                            from './components/Video.jsx'
+import { VideoContainer }                                                   from './components/VideoContainer.jsx'
 
 export {
   Badge,
@@ -19,6 +19,7 @@ export {
   CardAction,
   CardContent,
   CardImage,
+  CardReveal,
   CardTitle,
   Column,
   Container,
