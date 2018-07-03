b1685224b737233fcf27c0cc01c46fb25c21fbb5
Amin NAIRI <nairi.amin@gmail.com>
2018-07-03 08:33:43 +0200

    :bug: fixed rendering issues

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,36 +1,67 @@
 import { h } from 'hyperapp'
 
-const col = (prefix, value) => {
 
-  if (value && 1 <= value && value <= 12) {
-
-    return `${prefix}${value}`
-
-  }
-
-  return `${prefix}12`
-
-}
-
-export const Column = ({ small, medium, large, extraLarge, offsetSmall, offsetMedium, offsetLarge, offsetExtraLarge, className, ...attributes }, children) => {
+export const Column = ({ small, medium, large, extraLarge, offsetSmall, offsetMedium, offsetLarge, offsetExtraLarge, className, ...props }, children) => {
 
   return () => {
 
-    let classes = ['col']
+    let classes = []
 
-    classes.push(col('s', small))
-    classes.push(col('m', medium))
-    classes.push(col('l', large))
-    classes.push(col('xl', extraLarge))
-    classes.push(col('offset-s', offsetSmall))
-    classes.push(col('offset-m', offsetMedium))
-    classes.push(col('offset-l', offsetLarge))
-    classes.push(col('offset-xl', offsetExtraLarge))
+    classes.push('col')
+
+    if (small) {
+
+      classes.push(`s${small}`)
+
+    }
+
+    if (medium) {
+
+      classes.push(`m${medium}`)
+
+    }
+
+    if (large) {
+
+      classes.push(`l${large}`)
+
+    }
+
+    if (extraLarge) {
+
+      classes.push(`xl${extraLarge}`)
+
+    }
+
+    if (offsetSmall) {
+
+      classes.push(`offset-s${offsetSmall}`)
+
+    }
+
+    if (offsetMedium) {
+
+      classes.push(`offset-m${offsetMedium}`)
+
+    }
+
+    if (offsetLarge) {
+
+      classes.push(`offset-l${offsetLarge}`)
+
+    }
+
+    if (offsetExtraLarge) {
+
+      classes.push(`offset-xl${offsetExtraLarge}`)
+
+    }
+
     classes.push(className)
 
     return (
 
-      <div className={ classes.join(' ') } { ...attributes }>
+      <div className={ classes.join(' ') } { ...props }>
 
         { children }
 
