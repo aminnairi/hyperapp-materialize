0d4d44f35de4ea55caa3e46d55b9c23501a8c40f
Amin NAIRI <nairi.amin@gmail.com>
2018-07-06 23:31:39 +0200

    :sparkles: added the sticky modifier

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { h } from 'hyperapp'
 
-export const Footer = ({ className, ...props }, children) => {
+export const Footer = ({ className, sticky, ...props }, children) => {
 
   return () => {
 
@@ -14,9 +14,34 @@ export const Footer = ({ className, ...props }, children) => {
 
     classes.push('page-footer')
 
+    const stickyInitialization = element => {
+
+      if (sticky) {
+
+        const parent  = element.parentNode
+        const main    = document.querySelector('main')
+
+        if (parent) {
+
+          parent.style.display        = 'flex'
+          parent.style.flexDirection  = 'column'
+          parent.style.minHeight      = '100vh'
+
+        }
+
+        if (main) {
+
+          main.style.flex = '1 0 auto'
+
+        }
+
+      }
+
+    }
+
     return (
 
-      <footer className={ classes.join(' ') } { ...props }>
+      <footer className={ classes.join(' ') } { ...props } oncreate={ stickyInitialization }>
 
         { children }
 
